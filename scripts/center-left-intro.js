33de264211b65ce46509470b85404ba321c1669f
Muhammad Bilal <mubilal17@gmail.com>
2019-04-26 05:25:50 -0400

    initial commit

---
+++ scripts/center-left-intro.js
@@ -0,0 +1,11 @@
+var container = document.getElementById('left-intros');
+var children = container.children;
+
+var containerHeight = container.offsetHeight;
+var usedHeightByChildren = children[0].offsetHeight + children[1].offsetHeight + children[2].offsetHeight;
+
+var margin = (containerHeight - usedHeightByChildren)/2;
+
+var child0_bottom = children[0].offsetTop + children[0].offsetHeight;
+
+children[1].style.top = (child0_bottom + margin) + 'px';
